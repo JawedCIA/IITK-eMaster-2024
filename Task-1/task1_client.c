41d321c174d9aff609dc87d9a9dd58fbbea78c2a
Mohammed Jawed <jawed.ace@gmail.com>
2024-02-01 10:24:33 +0100

    Update task1_client.c

--- Task-1/task1_client.c
+++ Task-1/task1_client.c
@@ -12,8 +12,31 @@ void client(int readfd, int writefd){
    write client code to be used by the parent process
   *************************************************************/
   char buf[MAX_BUFF];
+  char buf_calc_result[MAX_BUFF];
   size_t len;
-
+	
+	printf("Enter your input : ");
+    fgets(buf, sizeof(buf), stdin);
   // implement client functionality
-
+  // Remove the newline character at the end if present
+            if (buf[strlen(buf) - 1] == '\n')
+            {
+                buf[strlen(buf) - 1] = '\0';
+            }
+	 // write pipe for client
+            write(writefd, buf, strlen(buf) + 1);
+            close(writefd);
+	 // Wait for child to send a string
+            wait(NULL);
+			 // Check for termination
+			if (strcmp(buf, "END") == 0) {
+				exit(0);
+			}
+	// Read string from child, print it and close
+            // reading end.
+            read(readfd, buf_calc_result, sizeof(buf_calc_result));
+            printf("RESULT: %s\n", buf_calc_result);
+            close(readfd);
+           // sleep(2);
+		wait(NULL);
 }

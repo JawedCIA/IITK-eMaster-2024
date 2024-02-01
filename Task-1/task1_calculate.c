af212906fabec0da76ba8c46df8cc4a9b31352a7
Mohammed Jawed <jawed.ace@gmail.com>
2024-02-01 10:23:32 +0100

    Update task1_calculate.c

--- Task-1/task1_calculate.c
+++ Task-1/task1_calculate.c
@@ -6,22 +6,58 @@
 #include <unistd.h>
 #include <calc.h>
 
-
-double calculate(char *buf){
+int main(int argc, char **argv){
 
   /*************************************************************
-   Implement the expression evaluation functionality which
-   will be invoked by the server whenever required.
+   write code for the main process that will establish
+   the communication channel between cleint and server using
+   pipes
   *************************************************************/
+	
+  pid_t pid;
 
-  double operands[20];
-  char op[20];
-  int oprnInd = 0;
-  double result = 0.0;
+  // TODO:
+  int fd1[2]; // Used to store two ends of first pipe
+  int fd2[2]; // Used to store two ends of second pipe
+  int loopCount;
+ for (loopCount = 0; loopCount < 10; loopCount++)
+  {
+	if (pipe(fd1) == -1)
+    {
+        fprintf(stderr, "Pipe Failed");
+        return 1;
+    }
 
-  // implement expression evaluation functionality here
+    if (pipe(fd2) == -1)
+    {
+        fprintf(stderr, "Pipe Failed");
+        return 1;
+    }
+	
+  pid = fork();
 
+  if(pid < 0){
+    perror("Fork failed\n");
+    return 1;
+  }
 
+  if(pid == 0){
 
-  return(result);
+    // TODO:
+	close(fd1[1]); // Close writing end of first pipe
+	 close(fd2[0]);
+	 server(fd1[0],fd2[1]);
+    //call to server here: server(int readfd, int writefd);
+    exit(0);
+  }
+
+  // TODO:
+	close(fd1[0]); // Close reading end of first pipe
+	close(fd2[1]); // Close writing end of second pipe
+	
+	client(fd2[0],fd1[1]);
+  //call to client here client(int readfd, int writefd);
+  } //End of For Loop
+  return 0;
+  
 }

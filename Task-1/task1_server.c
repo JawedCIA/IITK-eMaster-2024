1ca2bf3224c0f17ed555e3747d1acd931e228511
Mohammed Jawed <jawed.ace@gmail.com>
2024-02-01 10:24:50 +0100

    Update task1_server.c

--- Task-1/task1_server.c
+++ Task-1/task1_server.c
@@ -5,14 +5,33 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <calc.h>
+#include <time.h>
 
+time_t current_time;
+struct tm *time_info;
 void server(int readfd, int writefd){
   /*************************************************************
    write server code to be used by the child process
   *************************************************************/
   char buf[MAX_BUFF];
   size_t len;
-
+	char buf_result[MAX_BUFF];
+	double result;
+	read(readfd, buf, sizeof(buf));
+	// Check for termination
+			if (strcmp(buf, "END") == 0) {
+				exit(0);
+			}
+	close(readfd);
+	//Call Calculator
+	result=calculate(buf);
+	
+		// Using snprintf for safety
+    snprintf(buf_result, sizeof(buf_result), "%lf", result);
+    printf("Using snprintf: %s\n", buf_result);
+        write(writefd, buf_result, sizeof(buf_result));
+        close(writefd);
+        exit(0);
   /* Implement server functionlity here
     use calculate function to evaluate expression
     signature: calculate(char *buf)
